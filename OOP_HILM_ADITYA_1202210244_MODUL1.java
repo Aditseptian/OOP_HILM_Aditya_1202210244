b6c90f95146f60d2e27a6c47b7c353508c253a86
Aditseptian <92003257+Aditseptian@users.noreply.github.com>
2022-10-31 08:42:35 +0700

    Add files via upload

---
+++ OOP_HILM_ADITYA_1202210244_MODUL1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class database {
+    string insermenu ;
+    String showmenu ;
+    String seacrString;
+
+
+}
+
+public class Server{
+    Scanner scanner= new Scanner(System.in)
+    System.out.println("1.Masukan menu")
+    System.out.println ("2. Pilih menu")
+    System.out.println ("3. Keluar");
+    System.out.println("Masukan Pilihan 1/2/3/")
+
+}
+public class user {
+String Nama ;
+Integer No_Handphone ;
+public void setName(string Nama){
+    this.name  = name;
+}
+public void setNO(int No_Handphone ){
+    this.No = No_Handphone;
+}
+
+}
+
+    
+    
+
