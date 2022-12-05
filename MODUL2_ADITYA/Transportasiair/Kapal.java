005978fca66df2eb6b29ef3ad91097eb17cfbb7e
Aditseptian <92003257+Aditseptian@users.noreply.github.com>
2022-12-05 08:57:30 +0700

    Add files via upload

---
+++ MODUL2_ADITYA/Transportasiair/Kapal.java
@@ -0,0 +1,19 @@
+public class kapal extends Trasnportasiair {
+    protected String mesin;
+
+public kapal (int jumlahkursi,int biaya, string mesin){
+    this.mesin =mesin ;
+}
+@Override
+public void informasi(){
+    System.out.println("Transportasi AIR jenis kapal dengan jumlah kursi"+jumlahkuris+"Di tetapkan dengan biaya Sebesar"+biya+);
+
+}
+public void berlayar(){
+    System.out.println("Transportasi AIR jenis kapal dengan Mesin"+mesin);
+
+}
+public void berlayar1(){
+    System.out.print("");
+}
+}
